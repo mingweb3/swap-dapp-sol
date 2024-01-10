eaebe462fc0480b054ac4854b3893bd3885f2a44
mingweb3 <mingweb3@gmail.com>
2024-01-10 11:19:26 +0700

    feat: add ClientWalletProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import '@radix-ui/themes/styles.css'
 import { Theme } from '@radix-ui/themes'
 import { App } from './app'
 import { HelmetProvider } from 'react-helmet-async'
+import { ClientWalletProvider } from './contexts/ClientWalletProvider'
 
 import './styles/global.css'
 import './styles/theme-config.css'
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
       <Theme id="radix-root">
-        <App />
+        <ClientWalletProvider>
+          <App />
+        </ClientWalletProvider>
       </Theme>
     </HelmetProvider>
   </React.StrictMode>
