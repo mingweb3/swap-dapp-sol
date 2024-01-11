342dda626d46fb094170e263783633bc211c397d
mingweb3 <mingweb3@gmail.com>
2024-01-11 15:08:49 +0700

    Update code config build and compile code

--- vite.config.ts
+++ vite.config.ts
@@ -6,42 +6,65 @@ import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfil
 import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@/components": path.resolve(__dirname, "./src/app/components"),
-      "@/styles": path.resolve(__dirname, "./src/styles"),
-      "@/hooks": path.resolve(__dirname, "./src/hooks"),
-      "@/constants": path.resolve(__dirname, "./src/constants"),
-      "@/models": path.resolve(__dirname, "./src/models"),
-      "@/utils": path.resolve(__dirname, "./src/utils"),
-      "@/state": path.resolve(__dirname, "./src/state"),
-      "@/public/*": path.resolve(__dirname, "./public/*"),
-      crypto: 'crypto-browserify',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis'
+export default defineConfig(({ command  }) => {
+
+  // When developing on Local
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@/components": path.resolve(__dirname, "./src/app/components"),
+          "@/styles": path.resolve(__dirname, "./src/styles"),
+          "@/hooks": path.resolve(__dirname, "./src/hooks"),
+          "@/constants": path.resolve(__dirname, "./src/constants"),
+          "@/models": path.resolve(__dirname, "./src/models"),
+          "@/utils": path.resolve(__dirname, "./src/utils"),
+          "@/state": path.resolve(__dirname, "./src/state"),
+          "@/public/*": path.resolve(__dirname, "./public/*")
         },
-        // Enable esbuild polyfill plugins
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            buffer: true, 
-            process: true,
-          }), 
-          NodeModulesPolyfillPlugin() 
-        ]
+      }
     }
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        rollupNodePolyFill()
-      ]
+  } else {
+  // When Build and deploy
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@/components": path.resolve(__dirname, "./src/app/components"),
+          "@/styles": path.resolve(__dirname, "./src/styles"),
+          "@/hooks": path.resolve(__dirname, "./src/hooks"),
+          "@/constants": path.resolve(__dirname, "./src/constants"),
+          "@/models": path.resolve(__dirname, "./src/models"),
+          "@/utils": path.resolve(__dirname, "./src/utils"),
+          "@/state": path.resolve(__dirname, "./src/state"),
+          "@/public/*": path.resolve(__dirname, "./public/*"),
+          crypto: 'crypto-browserify',
+        },
+      },
+      optimizeDeps: {
+        esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+                global: 'globalThis'
+            },
+            // Enable esbuild polyfill plugins
+            plugins: [
+              NodeGlobalsPolyfillPlugin({
+                buffer: true, 
+                process: true,
+              }), 
+              NodeModulesPolyfillPlugin() 
+            ]
+        }
+      },
+      build: {
+        rollupOptions: {
+          plugins: [
+            rollupNodePolyFill()
+          ]
+        }
+      }
     }
   }
-});
+})
