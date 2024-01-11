8d20cc032f156770df49079fbcd9719c8af685b3
mingweb3 <mingweb3@gmail.com>
2024-01-11 17:30:16 +0700

    update swap program

--- src/constants/programs.ts
+++ src/constants/programs.ts
@@ -1,8 +1,23 @@
 import * as Web3 from '@solana/web3.js'
 
+// TOKEN A - B
 export const AAT_TOKEN_MINT = new Web3.PublicKey('DWiD4EVUtnsgqoGbdSK5kBjHRJ7XoGx58WPHBu7t73Dh')
 export const BBT_TOKEN_MINT = new Web3.PublicKey('4AG5yRYmMcpMxcRvvkLPHHiSdnCnRQhtncs79CoQNnRQ')
 
 // AIRDROP PROGRAM
 export const AIRDROP_PDA = new Web3.PublicKey('99ynLfSvcRXwYMKv4kmbcAyGxhfD7KfgrsuHTx9Dvoot')
 export const AIRDROP_PROGRAM_ID = new Web3.PublicKey('CPEV4ibq2VUv7UnNpkzUGL82VRzotbv2dy8vGwRfh3H3')
+
+// SWAP
+export const TOKEN_SWAP_PROGRAM_ID = new Web3.PublicKey('SwaPpA9LAaLfeLi3a68M4DjnLqgtticKg6CnyNwgAC8')
+export const TOKEN_SWAP_STATE_ACCOUNT = new Web3.PublicKey('EV7FEEq2EyTFtKx4ukp2QfW9mWLGcJckGGBNp5cjcHUe')
+
+export const SWAP_POOL_AUTHORITY = new Web3.PublicKey('24zqZMTYLVk4gm62seqU7KhBwvi3uBGtyDbnsC4rkbuR')
+
+// POOL
+export const POOL_TOKEN_MINT = new Web3.PublicKey('CXQYDT9ShDYG1JMMwjNiR6TcL4u4uJNnguAbLKw6jVv4')
+export const POOL_TOKEN_ACCOUNT = new Web3.PublicKey('Fp1W1KHuakombQATnToDCSpTnqLicFEfxWgtCAWbuvCM')
+export const POOL_TOKEN_FEE_ACCOUNT = new Web3.PublicKey('EY4hgx73saq9xuLr85HNaxGMAK6R5TkvuSDchKbpt291')
+
+export const AAT_POOL_TOKEN_ACCOUNT = new Web3.PublicKey('BVPUZrv5nk3jMyTWkZdxvp2LuyPF1DmGTyR8AzKvgZgN')
+export const BBT_POOL_TOKEN_ACCOUNT = new Web3.PublicKey('5ttkBtMndCbHdSib22K4wRUE5ifprPXkMSckzBRSQv3K')
